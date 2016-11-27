9651bc1655ed05f9efca40d94a18ff9783cd99a9
Bigbeer <Bigbeer>
2016-11-28 00:53:44 +0700

    edit login

--- loginc.php
+++ loginc.php
@@ -1,22 +1,38 @@
 
 <html>
 <head>
-
-	<title>Login</title>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="asset/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="shortcut icon" href="login-cus.ico">
+	<title>Login สำรับลูกค้า</title>
 </head>
 <body>
-	<?php 	
-		$hostname ="localhost";
-		$username ="cs57160040";
-		$password ="Dt8R7ASn2pAEuLVL";
-		$db_name="cs57160040";
-		$link = mysql_connect($hostname,$username,$password) or die(mysql_error());
-	 ?>
-	<form method="POST" action="List.php">
-		<input type="String" name="id" placeholder="username">
-		<input type="String" name="ps" placeholder="password">
-		<input type="Submit" value="เข้าสู่ระบบ">
+	<form method="POST" action="insert_login.php">
+		<div class="container">
+			<div style="width:600px; margin:0px auto;">
+				<div class="col-md-12 col-md-offset-2">
+					<br><img src="login.png" class="img-circle" width="270" height="220">
+				</div>
+				
+				<form method="POST" action="ddd.php">
+					 <div class="col-md-6 col-md-offset-2">
+					    <label for="exampleInputPassword1">Username</label>
+					    <input   class="form-control" name="username" placeholder="Username"><br>
+			  		</div>
+			  		<div class="col-md-6 col-md-offset-2">
+					    <label for="exampleInputPassword1">Password</label>
+					    <input  type="password" class="form-control" name="password" placeholder="Password"><br>
+			  		</div>
+			  		<div class="col-md-6 col-md-offset-6">
+			  			<button type="submit" class="btn btn-info">Log in</button>
+			  		</div>
+			  		
+				</form>
+			</div>
+		</div>
 	</form>
+	
 </body>
 </html>
